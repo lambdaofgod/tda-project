bfcd9b296e2be8d5642e3959e27c88d2ff3ca27c
lambdaofgod <bartczukkuba@gmail.com>
2017-09-01 14:53:48 +0200

    some cleanup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
+JAVAPLEX_VERSION=4.2.5
+JAVAPLEX_URL=https://github.com/appliedtopology/javaplex/releases/download/$JAVAPLEX_VERSION/javaplex-$JAVAPLEX_VERSION.jar
+
 mkdir -p lib
-wget -P lib https://github.com/appliedtopology/javaplex/releases/download/4.2.5/javaplex-4.2.5.jar
+wget -P lib 
